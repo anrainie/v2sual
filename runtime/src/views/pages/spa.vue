e4dad5ebc4604de7c1a23135ca77345406eee246
lijiancheng <lijiancheng@agree.com.cn>
2019-04-15 17:23:13 +0800

    页面流 -  部分10%
    页面流 -  部分10%

--- runtime/src/views/pages/spa.vue
+++ runtime/src/views/pages/spa.vue
@@ -1,66 +1,73 @@
+<!--@desp 单页页面 -->
 <template>
-	<section>
+  <section>
     <el-row>
-	<el-button type="success" @click="openNewPage">打开新页面</el-button>
-	<el-button type="primary" @click="openSubPage">打开子页面</el-button>
-	<el-button type="info" @click="openWindowPage">打开新窗口页面</el-button>
-	<el-button type="warning" @click="openSelfpage">打开自身页面</el-button>
-	</el-row>
-	</section>
+      <el-button type="success" @click="openNewPage">打开新页面</el-button>
+      <el-button type="primary" @click="openSubPage">打开子页面</el-button>
+      <el-button type="info" @click="openWindowPage">打开新窗口页面</el-button>
+      <el-button type="warning" @click="openSelfpage">打开自身页面</el-button>
+    </el-row>
+  </section>
 </template>
 <script>
-import { app } from '@/utils/app.js'
+import { app } from "@/utils/app.js";
 
-	export default {
-		data() {
-			return {
-			
-			}
-		},
-		methods: {
-			openNewPage(){
-					app.open({
-						status:true,
-						title:'新页面',
-						page:'pages/tree'
-					})
-			},
-			openSubPage(){
-						app.open({
-							status:true,
-							title:'子页面',
-							page:'pages/tree',
-							type:'SUB',
-							vm:this,//必填
-							confirmCallback:() => {
-								console.log('点击确定')
-							},
-							cancelCallback:(e) => {
-								console.log('点击取消')
-							}
-						})
-				
-			},
-			openWindowPage(){
-					app.open({
-								status:true,
-								title:'window页面',
-								page:'pages/table',
-								type:'WINDOW'
-							})
-			},
-			openSelfpage(){
-				  app.open({
-						status:true,
-						title:'自身页面',
-						page:'pages/tree',
-						type:'SELF'
-					})
-			}
-		},
-		mounted() {
-		   
-		}
-	};
-
+export default {
+  data() {
+    return {};
+  },
+  methods: {
+    /**
+     *  @desp 打开新页面A-Test
+     */
+    openNewPage() {
+      app.open({
+        status: true,
+        title: "新页面",
+        page: "pages/tree"
+      });
+    },
+    /**
+     *  @desp 打开子页面-Test
+     */
+    openSubPage() {
+      app.open({
+        status: true,
+        title: "子页面",
+        page: "pages/tree",
+        type: "SUB",
+        vm: this, //必填
+        confirmCallback: () => {
+          console.log("点击确定");
+        },
+        cancelCallback: e => {
+          console.log("点击取消");
+        }
+      });
+    },
+    /**
+     *  @desp 打开新窗口-Test
+     */
+    openWindowPage() {
+      app.open({
+        status: true,
+        title: "window页面",
+        page: "pages/table",
+        type: "WINDOW"
+      });
+    },
+    /**
+     *  @desp 覆盖自身页面-Test
+     */
+    openSelfpage() {
+      app.open({
+        status: true,
+        title: "自身页面",
+        page: "pages/tree",
+        type: "SELF"
+      });
+    }
+  },
+  mounted() {}
+};
 </script>
