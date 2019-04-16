07477bb77e80e7ebbc49bef075af748064e2da6b
gavin <gavin@git.com>
2019-04-16 14:51:47 +0800

    表单组件

---
+++ src/components/form/av-form-input/index.vue
@@ -0,0 +1,44 @@
+<template>
+  <div :style="model.style" ref="wrap" :class="wrapClass" style="display: flex" class="v2LableInput">
+    <label class="labelInput-label">{{model.data.label}}:</label>
+    <el-input
+      v-model="model.data.value"
+      style="flex:1"
+      :placeholder="model.data.placeholder"
+      @focus="focusEven"
+    ></el-input>
+  </div>
+</template>
+
+<script>
+import { widget, composite } from "../../../assets/js/v2-view.js";
+import { edit } from "../../../assets/js/edit.js";
+import inputField from "../../../assets/js/inputField.js";
+
+export default {
+  name: "v2LableInput",
+  mixins: [widget, inputField, edit],
+  mounted() {
+  },
+  computed: {
+    focusEven() {
+
+    }
+  },
+  methods: {
+    $ArrowRight() {
+      return true;
+    },
+    $ArrowLeft() {
+      return true;
+    }
+  }
+};
+</script>
+
+<style>
+.labelInput-label {
+  padding: 0 10px;
+  line-height: 40px;
+}
+</style>
---
+++ src/components/form/av-form-transfer/index.vue
@@ -0,0 +1,34 @@
+<template>
+  <div :style="model.style" ref="wrap" :class="wrapClass" class="v2Transfer">
+    <el-transfer
+      v-model="model.data.value"
+      :filterable="Boolean(model.data.filterable)"
+      :titles="[model.data.titleLeft, model.data.titleRight]"
+      :button-texts="[model.data.btnLeft,model.data.btnRight ]"
+      @change="handleChange"
+      :data="model.data.options">
+
+    </el-transfer>
+  </div>
+</template>
+
+<script>
+  import {widget, composite} from '../../../assets/js/v2-view.js'
+  import {edit} from '../../../assets/js/edit.js'
+
+  export default {
+    name: 'v2Transfer',
+    mixins: [widget, edit],
+    mounted () {
+    },
+    data () {
+      return {
+      }
+    },
+    methods: {
+      handleChange(e){
+        console.log(e)
+      }
+    },
+  }
+</script>
