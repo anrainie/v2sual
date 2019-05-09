ba60d4ca1a28053418bbb1f5bb2735932e3ce09b
KinSueng <594610825@qq.com>
2019-05-10 01:05:38 +0800

    afa、excel 代理改造
    afa、excel 代理改造

--- server/server.js
+++ server/server.js
@@ -93,21 +93,18 @@ app.use(koaBody());
 app.use(router.routes());
 
 
-
-const external = require('./external/external');
-platform.socket.on('/v1/external/widget',external.widget(platform));
-platform.socket.on('/v1/external/dict',external.dict(platform));
+require('./proxy/proxy')(app);
 
 //预览静态路由
-app.use(staticRouter([
-  {
-    router: '/v1/static/',     //dir:static resource directory
-    dir: path.resolve(path.join(RUNTIME_PATH, './dist'))   //router:router
-  },{
-    router:'/v1/runtime/',
-    dir: path.resolve(path.join(RUNTIME_PATH, './dist'))   //router:router
-  }
-]))
+// app.use(staticRouter([
+//   {
+//     router: '/v1/static/',     //dir:static resource directory
+//     dir: path.resolve(path.join(RUNTIME_PATH, './dist'))   //router:router
+//   },{
+//     router:'/v1/runtime/',
+//     dir: path.resolve(path.join(RUNTIME_PATH, './dist'))   //router:router
+//   }
+// ]))
 
 //异常处理
 app.on("error", (err, ctx) => {//捕获异常记录错误日志
