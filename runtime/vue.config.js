8b40e0169db712e00d8baf3dd779aa6fc66810a8
KinSueng <594610825@qq.com>
2019-05-20 18:32:24 +0800

    提交代理代码
    提交代理代码

--- runtime/vue.config.js
+++ runtime/vue.config.js
@@ -1,32 +1,30 @@
 var fs = require('fs');
 var path = require('path');
-var afa = require('./proxy/proxy.afa.js')
 var nodeServer = require('./proxy/proxy.node.js')
 module.exports = {
     publicPath: './',
     devServer: {
-          open: true,
-          host: '0.0.0.0',
-          port: 7007,
-          https: true,
-          disableHostCheck: true,
-          hotOnly: false,
-            proxy: {
-               ...afa,
-               ...nodeServer
-            }
-        
+        open: true,
+        host: '0.0.0.0',
+        port: 7007,
+        https: true,
+        disableHostCheck: true,
+        hotOnly: false,
+        proxy: {
+            ...nodeServer,
+        }
+
     },
     productionSourceMap: false,
     filenameHashing: false,
     css: {
-       
+
         // modules: false,
 
-       
+
         // extract: false,
 
-      
+
         // sourceMap: false
     }
 }
