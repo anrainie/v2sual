ab117094553e68fb7549c6db25f44d1dfc973a55
gavin <gavin@git.com>
2019-05-27 16:50:26 +0800

    数据绑定
    数据绑定

--- server/module/Reader/logicFile.js
+++ server/module/Reader/logicFile.js
@@ -7,18 +7,24 @@ const UglifyJS = require('uglify-es-web');
 let json2script = function (content) {
     let logic = JSON.parse(content).logic,
         data = JSON.parse(content).dataBasket.data,
-        datares = {},dataAst,dataAstContent,ast,dataTar,str,i,newData={},
+        datares = {}, dataAst, dataAstContent, ast, dataTar, str, i, newData = {},
         template;
     if (logic) {
-        dataAst = UglifyJS.parse("data="+JSON.stringify(data));
+        dataAst = UglifyJS.parse("data=" + JSON.stringify(data));
         dataAstContent = dataAst.body[0].body.right.properties;
 
         ast = UglifyJS.parse(logic);
         dataTar = ast.body[0].exported_value.properties.filter(item => item.key && item.key.name === "data")[0];
-        dataTar.value.body[0].value.properties = dataAstContent.map(item=>{
-            let ast = UglifyJS.parse("data="+data[item.key]).body[0].body.right;
+        dataTar.value.body[0].value.properties = dataAstContent.map(item => {
+            let ast;
+            if(data[item.key]===''){
+                 ast = UglifyJS.parse("data=''");
+            }else{
+                 ast = UglifyJS.parse("data=" + data[item.key]);
+            }
+                ast=ast.body[0].body.right;
             item.value = ast
-            return  item
+            return item
         });
         str = ast.print_to_string({ beautify: true, comments: true });
         return `<script>${str}</script>`
@@ -28,6 +34,51 @@ let json2script = function (content) {
     }
 }
 /**
+ * 绑定datas
+ * @param {String} htmlStr 
+ */
+let bindData = function (hook, structure) {
+    let list = [], item, newHook=[],tabIndex=[],
+        key = true;
+
+    // 检查
+    hook.value.body.map((item,index) => {
+        if(item.start.comments_before.length){
+            let start = item.start.comments_before.filter(item=>item.value==="*bind*");
+            if(start.length)tabIndex.push(index);
+        }
+        if(item.end.comments_after.length){
+            let end = item.end.comments_after.filter(item=>item.value==="*bind over*");
+            if(end.length)tabIndex.push(index);
+        }
+    });
+    hook.value.body = hook.value.body.filter((item,index)=>index<tabIndex[0]||index>tabIndex[1]);
+    item = structure;
+    while (key) {
+        if (item && item.component !== "av-layout-colctn" && item.component !== "v2Container" && item.component !== "av-layout-rowctn" && item.mapping) {
+            item.mapping.map(item => {
+                newHook.push(`this.$store.commit("bind",{ vueObj:this, data:${item.dataValue}, dataStr:"${item.dataValue}", wid:${item.id}, modelKey:"${item.modelValue}" });`)
+            })
+        }
+        if (item && item.children) list = list.concat(item.children);
+        if (list.length) {
+            item = list.pop();
+        } else {
+            key = false;
+        };
+    };
+    let template = `data = {beforeMout() {
+        /**bind**/ 
+        ${newHook}
+        /**bind over**/
+     }}`;
+     let tempAst = UglifyJS.parse(template);
+     let bindItems = tempAst.body[0].body.right.properties[0].value.body;
+     hook.value.body = hook.value.body.concat(bindItems);
+     return hook;
+}
+
+/**
  * 修改def json内容
  * @param {String} htmlStr 
  */
@@ -36,7 +87,7 @@ let changeDef = function (content, logicOptions) {
         name = logicOptions.name,
         code = logicOptions.code,
         type = logicOptions.type,
-        ast, astContent, methods,watch, str, tempAst, hook;
+        ast, astContent, methods, watch, str, tempAst, hook, beforeCreate;
 
     ast = UglifyJS.parse(logic);
     astContent = ast.body[0].exported_value.properties;
@@ -65,7 +116,7 @@ let changeDef = function (content, logicOptions) {
             break;
         default:
             methods = astContent.filter(item => item.key && item.key === "methods")[0];
-            methods.value.properties = methods.value.properties.map((item,index) => {
+            methods.value.properties = methods.value.properties.map((item, index) => {
                 if (item.key.name === name) {
                     return tempAst.body[0].body.right.properties[0];
                 } else {
@@ -73,7 +124,16 @@ let changeDef = function (content, logicOptions) {
                 }
             });
             break;
-    }
+    };
+    // 添加绑定数据
+    beforeCreate = astContent.map(item => {
+        if(item.key && item.key.name === "beforeCreate"){
+            return bindData(item, content.structure);
+        }else{
+            return item;
+        }
+    });
+    ast.body[0].exported_value.properties = beforeCreate;
     str = ast.print_to_string({ beautify: true, comments: true });
 
     content.logic = str;
