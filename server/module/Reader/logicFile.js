6a45acc9bb7bd25a30bb80c65491899fdf7915f6
gavin <gavin@git.com>
2019-05-28 18:18:57 +0800

    pipe
    pipe

--- server/module/Reader/logicFile.js
+++ server/module/Reader/logicFile.js
@@ -5,66 +5,36 @@ const UglifyJS = require('uglify-es-web');
  * @param {String} htmlStr 
  */
 let json2script = function (content) {
-  let logic = JSON.parse(content).logic;
-  let dataBasket = JSON.parse(content).dataBasket;
-  if(!logic||!dataBasket){
-      console.error('content缺少逻辑概览');
-      return `<script></script>`
-  }
-  let
-    data = dataBasket.data,
-    datares = {},
-    dataAst, dataAstContent, ast, dataTar, str, i, newData = {},
-    template;
-  if (logic) {
-    dataAst = UglifyJS.parse("data=" + JSON.stringify(data));
-    dataAstContent = dataAst.body[0].body.right.properties;
+    let logic = JSON.parse(content).logic,
+        template;
+    if (logic) {
 
-    ast = UglifyJS.parse(logic);
-    dataTar = ast.body[0].exported_value.properties.filter(item => item.key && item.key.name === "data")[0];
-    dataTar.value.body[0].value.properties = dataAstContent.map(item => {
-      let ast;
-      if (data[item.key] === '') {
-        ast = UglifyJS.parse("data=''");
-      } else {
-        ast = UglifyJS.parse("data=" + data[item.key]);
-      }
-      ast = ast.body[0].body.right;
-      item.value = ast
-      return item
-    });
-    str = ast.print_to_string({
-      beautify: true,
-      comments: true
-    });
-    return `<script>${str}</script>`
-  } else {
-    template = `export default{data(){return{}},methods:{},watch:{}}`
-    return `<script>${template}</script>`
-  }
+        return `<script>${logic}</script>`
+    } else {
+        template = `export default{data(){return{}},methods:{},watch:{}}`
+        return `<script>${template}</script>`
+    }
 }
 /**
  * 绑定datas
  * @param {String} htmlStr 
  */
 let bindData = function (hook, structure) {
-  let list = [],
-    item, newHook = [],
-    tabIndex = [],
-    key = true;
+    let list = [], item, newHook = [], tabIndex = [],
+        key = true;
 
     // 检查
-    hook.value.body.map((item,index) => {
-        if(item.start.comments_before.length){
-            let start = item.start.comments_before.filter(item=>item.value==="*bind*");
-            if(start.length)tabIndex.push(index);
+    hook.value.body.map((item, index) => {
+        if (item.start.comments_before.length) {
+            let start = item.start.comments_before.filter(item => item.value === "*bind*");
+            if (start.length) tabIndex.push(index);
         }
-        if(item.end.comments_after.length){
-            let end = item.end.comments_after.filter(item=>item.value==="*bind over*");
-            if(end.length)tabIndex.push(index);
+        if (item.end.comments_after.length) {
+            let end = item.end.comments_after.filter(item => item.value === "*bind over*");
+            if (end.length) tabIndex.push(index);
         }
     });
-    if(tabIndex.length) hook.value.body = hook.value.body.filter((item,index)=>index<tabIndex[0]||index>tabIndex[1]);
+    if (tabIndex.length) hook.value.body = hook.value.body.filter((item, index) => index < tabIndex[0] || index > tabIndex[1]);
     item = structure;
     while (key) {
         if (item && item.component !== "av-layout-colctn" && item.component !== "v2Container" && item.component !== "av-layout-rowctn" && item.mapping) {
@@ -79,16 +49,15 @@ let bindData = function (hook, structure) {
             key = false;
         };
     };
-  };
-  let template = `data = {beforeMout() {
+    let template = `data = {beforeMout() {
         /**bind**/ 
         ${newHook}
         /**bind over**/
      }}`;
-  let tempAst = UglifyJS.parse(template);
-  let bindItems = tempAst.body[0].body.right.properties[0].value.body;
-  hook.value.body = hook.value.body.concat(bindItems);
-  return hook;
+    let tempAst = UglifyJS.parse(template);
+    let bindItems = tempAst.body[0].body.right.properties[0].value.body;
+    hook.value.body = hook.value.body.concat(bindItems);
+    return hook;
 }
 
 /**
@@ -100,67 +69,77 @@ let changeDef = function (content, logicOptions) {
         name = logicOptions.name,
         code = logicOptions.code,
         type = logicOptions.type,
+        data = content.dataBasket.data,
         ast, astContent, methods,
         watch, str, tempAst, hook,
-        beforeCreate,defini;
+        dataAst, dataAstContent, dataTar,
+        beforeCreate, defini;
 
     ast = UglifyJS.parse(logic);
     astContent = ast.body[0].exported_value.properties;
     tempAst = UglifyJS.parse(`data={${code}}`);
     defini = tempAst.body[0].body.right.properties[0].value.body[0];
-    if(defini&&defini.definitions&&defini.definitions[0].name.name==="ctx")tempAst.body[0].body.right.properties[0].value.body[0].end.comments_after=[];
-
+    if (defini && defini.definitions && defini.definitions[0].name.name === "ctx") tempAst.body[0].body.right.properties[0].value.body[0].end.comments_after = [];
+    // 处理data
+    dataAst = UglifyJS.parse("data=" + JSON.stringify(data));
+    dataAstContent = dataAst.body[0].body.right.properties;
+    dataTar = ast.body[0].exported_value.properties.filter(item => item.key && item.key.name === "data")[0];
+    dataTar.value.body[0].value.properties = dataAstContent.map(item => {
+        let ast;
+        if (data[item.key] === '') {
+            ast = UglifyJS.parse("data=''");
+        } else {
+            ast = UglifyJS.parse("data=" + data[item.key]);
+        }
+        ast = ast.body[0].body.right;
+        item.value = ast
+        return item
+    });
     switch (type) {
-      case "hook":
-        hook = astContent.map(item => {
-          if (item.key && item.key.name === name) {
-            return tempAst.body[0].body.right.properties[0];
-          } else {
-            return item;
-          }
-        });
-        ast.body[0].exported_value.properties = hook;
-        break;
-      case "watch":
-        watch = astContent.filter(item => item.key && item.key === "watch")[0];
-        watch.value.properties = watch.value.properties.map(item => {
-          if (item.key.name === name) {
-            return tempAst.body[0].body.right.properties[0];
-          } else {
-            return item;
-          }
-        });
-        break;
-      default:
-        methods = astContent.filter(item => item.key && item.key === "methods")[0];
-        methods.value.properties = methods.value.properties.map((item, index) => {
-          if (item.key.name === name) {
-            return tempAst.body[0].body.right.properties[0];
-          } else {
-            return item;
-          }
-        });
-        break;
+        case "hook":
+            hook = astContent.map(item => {
+                if (item.key && item.key.name === name) {
+                    return tempAst.body[0].body.right.properties[0];
+                } else {
+                    return item;
+                }
+            });
+            ast.body[0].exported_value.properties = hook;
+            break;
+        case "watch":
+            watch = astContent.filter(item => item.key && item.key === "watch")[0];
+            watch.value.properties = watch.value.properties.map(item => {
+                if (item.key.name === name) {
+                    return tempAst.body[0].body.right.properties[0];
+                } else {
+                    return item;
+                }
+            });
+            break;
+        default:
+            methods = astContent.filter(item => item.key && item.key === "methods")[0];
+            methods.value.properties = methods.value.properties.map((item, index) => {
+                if (item.key.name === name) {
+                    return tempAst.body[0].body.right.properties[0];
+                } else {
+                    return item;
+                }
+            });
+            break;
     };
-  }
-  if (astContent) {
     // 添加绑定数据
     beforeCreate = ast.body[0].exported_value.properties.map(item => {
-        if(item.key && item.key.name === "beforeCreate"){
+        if (item.key && item.key.name === "beforeCreate") {
             return bindData(item, content.structure);
-        }else{
+        } else {
             return item;
         }
     });
     ast.body[0].exported_value.properties = beforeCreate;
-    str = ast.print_to_string({
-      beautify: true,
-      comments: true
-    });
+    str = ast.print_to_string({ beautify: true, comments: true });
 
     content.logic = str;
-  }
-  return content;
+    return content;
 }
 
 //======================================================================================================================
