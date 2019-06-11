63285f390ef3318a7c5229d2dd3953f53140a319
ZhangErin <31231134+ZhangErin@users.noreply.github.com>
2019-06-11 10:35:12 +0800

    恢复被覆盖的预览首页布局，重新打包了一份组件

---
+++ runtime/src/@aweb-components/v2-component-text/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div :class="model.layoutClass" :contenteditable="model.edit"></div>
+</template>
+<script>
+  export default {
+    name: 'v2-component-text',
+
+    data(){
+      return {
+        
+      }
+    },
+    mounted() {
+    
+    }
+  }
+</script>
+<style lang="less" scoped>
+.echartCpt{
+  height: 400px;
+}
+</style>
