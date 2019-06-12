6d8fe6edc4c4154a8ab22df8d9d1ed2109be6abf
ZhangErin <31231134+ZhangErin@users.noreply.github.com>
2019-06-12 11:52:47 +0800

    更新页面

--- runtime/server.js
+++ runtime/server.js
@@ -131,86 +131,76 @@ router.get('/-/user/add', (ctx) => {
 });
 //路由列表
 const routerList = [
-		{
-			"path": "open",
-			"name": "open",
-			"component": "spa/open",
-			"meta": {
-				"title": "OPEN",
-				"icon": "el-icon-goods"
-			}
-		},
 	{
-		"path": "spa",
-		"name":"spa",
-		"component": "spa/spa",
+		"path": "open",
+		"name": "open",
+		"component": "spa/open",
 		"meta": {
-			"title": "SPA",
+			"title": "OPEN",
 			"icon": "el-icon-goods"
 		}
 	},
-	{
-		"path": "case",
-		"name": "case",
-		"meta": {
-			"title": "案例",
-			"icon": "el-icon-tickets"
-		},
-		"children": [
-			{
-				"path": "table",
-				"name": "table",
-				"component": "example/table",
-				"meta": {
-					"title": "表格",
-					"icon": "el-icon-goods",
-					"keepAlive":true
-				}
-			}, {
-				"path": "rightMenu",
-				"name": "rightMenu",
-				"component": "example/rightMenu",
-				"meta": {
-					"title": "右键菜单",
-					"icon": "el-icon-menu"
-				}
-			}, {
-				"path": "form",
-				"name": "form",
-				"component": "form/form",
-				"meta": {
-					"title": "表单",
-					"icon": "el-icon-setting"
-				}
-			}, {
-				"path": "hasSubTab",
-				"name": "hasSubTab",
-				"component": "example/hasSubTab",
-				"meta": {
-					"title": "嵌套子页面",
-					"icon": "el-icon-setting",
-					"keepAlive":true
-				}
-			}, {
-				"path": "dycSubTab",
-				"name": "dycSubTab",
-				"component": "example/dycSubTab",
-				"meta": {
-					"title": "测试",
-					"icon": "el-icon-setting"
-				}
-			}, {
-				"path": "afaAjax",
-				"name": "afaAjax",
-				"component": "example/afaAjax",
-				"meta": {
-					"title": "afa直连",
-					"icon": "el-icon-setting"
-				}
-			}
-			
-		]
+{
+	"path": "spa",
+	"name":"spa",
+	"component": "spa/spa",
+	"meta": {
+		"title": "SPA",
+		"icon": "el-icon-goods"
 	}
+},
+{
+	"path": "case",
+	"name": "case",
+	"meta": {
+		"title": "案例",
+		"icon": "el-icon-tickets"
+	},
+	"children": [
+		{
+			"path": "table",
+			"name": "table",
+			"component": "example/table",
+			"meta": {
+				"title": "表格",
+				"icon": "el-icon-goods"
+			}
+		}, {
+			"path": "rightMenu",
+			"name": "rightMenu",
+			"component": "example/rightMenu",
+			"meta": {
+				"title": "右键菜单",
+				"icon": "el-icon-menu"
+			}
+		}, {
+			"path": "form",
+			"name": "form",
+			"component": "form/form",
+			"meta": {
+				"title": "表单",
+				"icon": "el-icon-setting"
+			}
+		}, {
+			"path": "hasSubTab",
+			"name": "hasSubTab",
+			"component": "example/hasSubTab",
+			"meta": {
+				"title": "嵌套子页面",
+				"icon": "el-icon-setting"
+			}
+		}, {
+			"path": "dycSubTab",
+			"name": "dycSubTab",
+			"component": "example/dycSubTab",
+			"meta": {
+				"title": "动态子页面",
+				"icon": "el-icon-setting"
+			}
+		}
+		
+	]
+}
 ];
 
 
