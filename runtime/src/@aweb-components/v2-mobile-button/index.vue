7e675526e00656d088b9aa929bf5219fc4f09aa1
gavin <gavin@git.com>
2019-06-24 14:04:55 +0800

    移动端组件
    移动端组件

---
+++ runtime/src/@aweb-components/v2-mobile-button/index.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="v2-form-ctt v2-mobile-button" :class="model.layoutClass">
+      <van-button 
+     :type="model.type"
+     :disabled="model.disabled"
+     :loading ="model.loading "
+     :round ="model.round "
+     :icon ="model.icon "
+     :size ="model.size "
+     >{{model.text}}</van-button>
+  </div>
+</template>
+
+<script>
+import Vue from "vue";
+import Vant from "vant";
+import "vant/lib/index.css";
+
+Vue.use(Vant);
+
+export default {
+  name: "v2-mobile-button",
+
+  mounted() {},
+  data() {
+    return {};
+  },
+  methods: {},
+  computed: {}
+};
+</script>
+
+<style>
+</style>
---
+++ runtime/src/@aweb-components/v2-mobile-input/index.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="v2-form-ctt v2-mobile-input" :class="model.layoutClass">
+      <van-field
+        v-model="model.value"
+        :placeholder="model.placeholder"
+        :label="model.label"
+        :disabled="model.disabled"
+        :required="model.required"
+        :readonly="model.readonly"
+      />
+  </div>
+</template>
+
+<script>
+import Vue from "vue";
+import Vant from "vant";
+import "vant/lib/index.css";
+
+Vue.use(Vant);
+
+export default {
+  name: "v2-mobile-input",
+
+  mounted() {},
+  data() {
+    return {};
+  },
+  methods: {},
+  computed: {}
+};
+</script>
+
+<style>
+</style>
