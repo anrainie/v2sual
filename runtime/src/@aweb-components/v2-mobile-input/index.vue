a42ae69d95cafe69648f9dfc9776af1f16b27b38
gavin <gavin@git.com>
2019-06-25 15:32:13 +0800

    新添组件
    新添组件

--- runtime/src/@aweb-components/v2-mobile-input/index.vue
+++ runtime/src/@aweb-components/v2-mobile-input/index.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="v2-form-ctt v2-mobile-input" :class="model.layoutClass">
+    <van-cell-group>
       <van-field
         v-model="model.value"
         :placeholder="model.placeholder"
@@ -7,7 +8,15 @@
         :disabled="model.disabled"
         :required="model.required"
         :readonly="model.readonly"
-      />
+      >
+      <van-button 
+      v-if="model.button"
+      slot="button" 
+      :size="model.buttonSize" 
+      :type="model.buttonType"
+      >{{model.buttonText}}</van-button>
+      </van-field>
+      </van-cell-group>
   </div>
 </template>
 
