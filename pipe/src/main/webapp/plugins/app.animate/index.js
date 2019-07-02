e3f9c34e04c25ab6fbf657b0678c7aac42e74874
lijiancheng <lijiancheng@agree.com.cn>
2019-07-02 11:32:49 +0800

    调整页面动画效果
    调整页面动画效果

--- pipe/src/main/webapp/plugins/app.animate/index.js
+++ pipe/src/main/webapp/plugins/app.animate/index.js
@@ -1,13 +1,27 @@
 import 'animate.css';
 
-export default (element, animate = '', delay = '', speed = '', callback, loop = false) => {
-    const c = ['animated', animate, delay, speed, loop ? 'infinite' : ''];
-    const handleAnimationEnd = () => {
-        element.classList.remove.apply(element.classList, c);
-        element.removeEventListener('animationend', handleAnimationEnd)
+export default (element, animate = '', delay = '', speed = '', loop = 1) => {
 
-        if (typeof callback === 'function') callback()
-    }
-    element.classList.add.apply(element.classList, c);
-    element.addEventListener('animationend', handleAnimationEnd);
+    return new Promise(async res => {
+        const c = ['animated', animate, delay, speed, loop === -1 ? 'infinite' : ''].filter(e=>!!e);
+
+        //假设是循环
+        if (loop < 0) {
+            element.classList.add.apply(element.classList, c);
+            res('animationend');
+        } else if (loop > 0) {
+            const handleAnimationEnd = () => {
+                element.classList.remove.apply(element.classList, c);
+                if (--loop) {
+                    element.classList.add.apply(element.classList, c);
+                } else {
+                    element.removeEventListener('animationend', handleAnimationEnd)
+                    res('animationend');
+                }
+            }
+            element.classList.add.apply(element.classList, c);
+            element.addEventListener('animationend', handleAnimationEnd);
+        }
+    })
+
 }
