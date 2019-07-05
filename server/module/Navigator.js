28e7b4bdec24caa272a0c94e7ff73bd404c0e669
gavin <gavin@git.com>
2019-07-05 18:15:24 +0800

    项目资源
    项目资源

--- server/module/Navigator.js
+++ server/module/Navigator.js
@@ -85,6 +85,29 @@ const listDir = (dirPath = listPath) => {
 
 
             treeNode.push(item);
+          } else if (path.extname(filePath) === '.png' || path.extname(filePath) === '.jpg') {
+            const imgInfo = path.parse(filePath);
+            let item = {
+              name: imgInfo.base,
+              label: imgInfo.base,
+              resId: 'img',
+              icon: "ideicon iconyemian",
+              type: 'file',
+              category: 100,
+              path: relativePath
+            };
+            try {
+              const defStr = fs.readFileSync(`${filePath}.def`, { encoding: 'utf8' }).toString();
+              const def = JSON.parse(defStr);
+
+              if (def.display && def.display.desp) {
+                item.desp = def.display.desp;
+              }
+
+            } catch (e) {
+
+            }
+            treeNode.push(item);
           }
         }
         treeNode.sort(sorter);
@@ -94,6 +117,142 @@ const listDir = (dirPath = listPath) => {
     console.error(e);
   }
 
+  return treeNode;
+}
+const statisDir = () => {
+  const stylePath = path.join(config.runtime.base, '\\src\\less');
+  const statisPath = path.join(config.runtime.base, '\\public');
+  let treeNode = [
+    {
+      name: 'style',
+      label: '自定义样式',
+      path: '\\src\\less',
+      resId: 'category',
+      desp: '\\src\\less',
+      type: 'folder',
+      category: 50,
+      children: []
+    },
+    {
+      name: 'publicResources',
+      label: '公共资源',
+      path: '\\public',
+      desp: '\\public',
+      resId: 'category',
+      type: 'folder',
+      category: 50,
+      children: []
+    }
+  ];
+  //style
+  try {
+    const stats = fs.lstatSync(stylePath);
+    if (stats.isDirectory()) {
+      let files = fs.readdirSync(stylePath);
+      for (let i = 0; i < files.length; i++) {
+        let filePath = path.join(stylePath, files[i]);
+        let relativePath = trans2RelativePath(filePath);
+        let stat = fs.lstatSync(stylePath + path.sep + files[i]);
+        if (path.extname(filePath) === '.less') {
+          let fileInfo = path.parse(filePath);
+          let item = {
+            name: fileInfo.base,
+            label: fileInfo.base,
+            resId: 'style',
+            icon: "ideicon iconyemian",
+            type: 'file',
+            category: 100,
+            path: relativePath
+          };
+          treeNode[0].children.push(item);
+        }
+      }
+    }
+    //statis
+    treeNode[1].children = scanDir(statisPath);
+  } catch (e) {
+    console.error(e);
+  }
+  return treeNode;
+}
+
+const scanDir = (dirPath) => {
+  let treeNode = [];
+  try {
+    const stats = fs.lstatSync(dirPath);
+    if (stats.isDirectory()) {
+      let files = fs.readdirSync(dirPath);
+      for (let i = 0; i < files.length; i++) {
+        let filePath = path.join(dirPath, files[i]);
+        let relativePath = trans2RelativePath(filePath);
+        let stat = fs.lstatSync(dirPath + path.sep + files[i]);
+        if (stat.isDirectory()) {
+          let DirInfo = scanDir(filePath);
+          let name = path.basename(filePath);
+
+          let folder = {
+            name,
+            label: name,
+            path: relativePath,
+            resId: 'category',
+            type: 'folder',
+            category: 50,
+            children: DirInfo
+          };
+
+          //特殊处理某些文件夹
+          switch (name) {
+            case CATEGORY.ENTRY.NAME:
+              folder.entry = true;
+              folder.label = CATEGORY.ENTRY.LABEL;
+              folder.index = -2;
+              folder.icon = "ideicon iconrukouwenjianjia";
+              break;
+            case CATEGORY.CUSTOM_WIDGET.NAME:
+              folder.label = CATEGORY.CUSTOM_WIDGET.LABEL;
+              folder.index = -1;
+              folder.icon = "ideicon iconzidingyizujianwenjianjia";
+
+              break;
+            default:
+              folder.index = 0;
+          }
+          treeNode.push(folder);
+        } else {
+          if (!EXCLUDE_MAP[filePath]) {
+            const nodeInfo = path.parse(filePath);
+            let item = {
+              name: nodeInfo.base,
+              label: nodeInfo.base,
+              resId: 'statis',
+              icon: "ideicon iconyemian",
+              type: 'file',
+              category: 100,
+              path: relativePath
+            };
+
+            try {
+              const defStr = fs.readFileSync(`${filePath}.def`, { encoding: 'utf8' }).toString();
+              const def = JSON.parse(defStr);
+
+              if (def.display && def.display.desp) {
+                item.desp = def.display.desp;
+              }
+
+            } catch (e) {
+
+            }
+
+
+            treeNode.push(item);
+          }
+        }
+        treeNode.sort(sorter);
+      }
+    }
+  } catch (e) {
+    console.error(e);
+  }
   return treeNode;
 }
 
@@ -125,8 +284,10 @@ const Navigator = {
   getRootItems(platform, dirPath, req) {
     try {
       const viewPath = listDir(dirPath);
+      const imgPath = path.join(config.runtime.base, '\\public\\img');
+      // const imgList = listDir(imgPath);
       let flowPath = JSON.parse(JSON.stringify(viewPath.filter(v => v.entry).map(e => e.children)[0] || []));
-
+      let imgList  = statisDir();
       //rename flow
       let copy = [].concat(flowPath);
       let item;
@@ -156,8 +317,15 @@ const Navigator = {
         resId: 'pathFlow',
         type: 'folder',
         children: flowPath
-      }];
-
+      }, {
+        name: 'statis',
+        label: '项目资源',
+        path: path.join(config.runtime.base, '\\public\\img'),
+        resId: 'pathImg',
+        type: 'folder',
+        children: imgList
+      }
+      ];
       platform.sendSuccessResult(req, ret);
     } catch (e) {
       platform.sendErrorResult(req, e)
