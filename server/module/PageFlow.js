819126caa748a45c09bd89b273fbe09341fce01e
gavin <gavin@git.com>
2019-07-11 09:40:40 +0800

    添加主题
    添加主题

--- server/module/PageFlow.js
+++ server/module/PageFlow.js
@@ -522,18 +522,19 @@ class Page {
         let base64Data = req.data.params.result.replace(/^data:image\/\w+;base64,/, "");
         let dataBuffer = new Buffer(base64Data, 'base64');
         let staticPath = trans2absolute(`/public/img`);
-        let path = trans2absolute(`/public/img/${req.data.params.name}`);
+        let filePath = trans2absolute(`/public/img/${req.data.params.name}`);
         let distPath = trans2absolute(`/dist/img/${req.data.params.name}`);
+        let imgPath = path.join('/public','img')
         // 检测路径
          fs.exists(staticPath, async(exists) => {
           if (!exists) {
             fs.mkdirSync(staticPath);
           }
 
-          let publishDown = await context.writeimg(path,dataBuffer);
+          let publishDown = await context.writeimg(filePath,dataBuffer);
           let distDown = await context.writeimg(distPath,dataBuffer);
           if(publishDown&&distDown){
-            platform.sendSuccessResult(req, { status: true,path:path });
+            platform.sendSuccessResult(req, { status: true,path:imgPath });
           }else{
             platform.sendErrorResult(req, "保存失败");
           }
