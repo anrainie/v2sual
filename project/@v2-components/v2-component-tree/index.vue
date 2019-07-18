afda7e9abfa6d622f05f8a655c06074b89fc66b0
anrainie <anrainie@163.com>
2019-07-18 20:13:30 +0800

    tree

--- project/@v2-components/v2-component-tree/index.vue
+++ project/@v2-components/v2-component-tree/index.vue
@@ -1,42 +1,44 @@
 <template>
- 
-    <div :class="widgetClass" :style="{...(model.style && model.style.treeWarpStyle),...model.commonStyle}">
-      <el-tree :data="model.configs||[]" :props="model.defaultProps"  ref="_op_componentTree_tree"
+  <div
+    :class="widgetClass"
+    :style="{...(model.style && model.style.treeWarpStyle),...model.commonStyle}"
+  >
+    <el-tree
+      :data="myconfig"
+      :props="model.defaultProps"
+      ref="_op_componentTree_tree"
       :show-checkbox="model.showCheckbox"
       :node-key="model.nodeKey||null"
       :default-expanded-keys="model.defExpanded||[]"
       :default-checked-keys="model.defChecked||[]"
-      ></el-tree>
-    </div>
-
+    ></el-tree>
+  </div>
 </template>
 <script>
-
 export default {
   name: "v2-component-tree",
 
   data() {
-    return {
-    
-    };
+    return {};
   },
   computed: {
+    myconfig() {
+      if (this.model.configs && this.model.configs.constructor == Array) {
+        return this.model.configs;
+      }
+      return [];
+    },
     extraClass() {
       return this.error ? "echart-error" : "echart-normal";
     }
   },
-  watch: {
-
-  },
-  methods: {
-
-  },
+  watch: {},
+  methods: {},
   mounted() {}
 };
 </script>
 
 <style lang="scss" scoped>
-
 </style>
 
 
