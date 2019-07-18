0a707c28a4a3dfa0d56acf947da11d807f091e82
lijiancheng <lijiancheng@agree.com.cn>
2019-07-18 17:50:12 +0800

    提交代码
    提交代码

--- project/cms/vue.config.js
+++ project/cms/vue.config.js
@@ -1,14 +1,15 @@
 module.exports = {
-  baseUrl: './',
-
+  publichPath:'/cms/',
   devServer: {
     port: 7009,
+    sockPort:443,
+    sockPath:'/cms/sockjs-node',
     disableHostCheck: true,
     hotOnly: false,
     proxy: {
       //假数据
       '/mock': {
-        target: 'http://localhost:7010',
+        target: 'http://localhost:7011',
         changeOrigin: true,     // target是域名的话，需要这个参数，
         secure: false,          // 设置支持https协议的代理
         pathRewrite: {
@@ -22,7 +23,7 @@ module.exports = {
   css: {
     modules: false,
     extract: false,
-    sourceMap: false,
+  sourceMap: false,
     loaderOptions: {
       sass: {
         // 向全局sass样式传入共享的全局变量
