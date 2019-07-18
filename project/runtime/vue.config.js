9abc0bd44bf0a806a780db5e7fed9e0eaccf6f97
ZhangErin <31231134+ZhangErin@users.noreply.github.com>
2019-07-18 11:53:52 +0800

    fix 预览 bug

--- project/runtime/vue.config.js
+++ project/runtime/vue.config.js
@@ -1,20 +1,17 @@
 
             module.exports = {
                 baseUrl: './',
-      
                 devServer: {
                   port: 7007,
                   disableHostCheck: true,
                   hotOnly: false,
                   proxy: {
-                    //假数据
-                    '/mock':{
+                   
+                    '/':{
                       target: 'http://localhost:7008',
                       changeOrigin: true,     // target是域名的话，需要这个参数，
-                      secure: false,          // 设置支持https协议的代理
-                      pathRewrite: {
-                        '/mock': '/'
-                      }
+                      secure: false          // 设置支持https协议的代理
+               
                     }
                   }
                 },
