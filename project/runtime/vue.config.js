7afc868799e7dda57b08980f8996eaa40bc73c76
lijiancheng <lijiancheng@agree.com.cn>
2019-07-29 18:13:29 +0800

    增加项目gzip压缩
    增加项目gzip压缩

--- project/runtime/vue.config.js
+++ project/runtime/vue.config.js
@@ -1,5 +1,3 @@
-
-
 //获取配置
 const getConfig = () => {
 
@@ -33,13 +31,14 @@ module.exports = {
     sockPath: config.sockPath,
     disableHostCheck: true,
     hotOnly: false,
+    compress: true,
     proxy: {
       //假数据
       '/': {
         target: `http://localhost:${config.mockPort}`,
         changeOrigin: true,     // target是域名的话，需要这个参数，
         secure: false,          // 设置支持https协议的代理
-        ws:false,
+        ws: false,
         pathRewrite: {
           '/mock': ''
         }
