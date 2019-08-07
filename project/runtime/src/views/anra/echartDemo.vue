49b295418ee1bde2069f8752ff2e0b81242c4f12
anrainie <anrainie@163.com>
2019-08-07 09:59:00 +0800

    demo

---
+++ project/runtime/src/views/anra/echartDemo.vue
@@ -0,0 +1,103 @@
+<template><v2container :wid="`root`"><el-row class="V2ContainerBlock" style="height:15%;width:100%;" key="0"><v2empty class="V2Empty" id="root-0" :wid="`root-0`" :index="0" :pid="`root`"></v2empty></el-row><el-row class="V2ContainerBlock borderBox dashBorder" style="height:85%;width:100%;" key="1"><v2-component-echart class="V2Widget" id="1565088728187" :wid="`1565088728187`" :index="1" :pid="`root`"></v2-component-echart></el-row></v2container></template>
+<script>
+            import {root} from '@/utils/v2-view';
+            
+            export default{
+                data(){
+                    return{
+                        "CONTENT":{structure:{"id":"root","component":"v2Container","direction":"row","layout":[15,85],"style":{"width":"100%","height":"100%"},"data":{},"children":[null,{"configs":"${echartConfig}","ctnClass":"","customClass":"","__type":"","__loopTarget":"","__capacity":"1","component":"v2-component-echart","href":"v2-component-echart","children":[],"events":{},"id":1565088728187,"pid":"root","commonStyle":{"position":"","top":"","right":"","left":"","bottom":"","width":"","height":"","padding-top":"","padding-bottom":"","padding-left":"","padding-right":"","margin-top":"","margin-bottom":"","margin-left":"","margin-right":"","font-size":"","color":""}}],"events":{},"realSize":["%","%"],"ctnStyle":[{"position":"","top":"","right":"","left":"","bottom":"","width":"","height":"","text-align":"","background-color":"","padding-top":"","padding-bottom":"","padding-left":"","padding-right":"","margin-top":"","margin-bottom":"","margin-left":"","margin-right":"","border-color":"","border-width":"","border-style":""},{"position":"","top":"","right":"","left":"","bottom":"","width":"","height":"","text-align":"","background-color":"","padding-top":"","padding-bottom":"","padding-left":"","padding-right":"","margin-top":"","margin-bottom":"","margin-left":"","margin-right":"","border-color":"","border-width":"","border-style":""}]}},
+                        echartConfig:{
+    angleAxis: {
+    },
+    radiusAxis: {
+        type: 'category',
+        data: ['周一', '周二', '周三', '周四'],
+        z: 10
+    },
+    polar: {
+    },
+    series: [{
+        type: 'bar',
+        data: [1, 2, 3, 4],
+        coordinateSystem: 'polar',
+        name: 'A',
+        stack: 'a'
+    }, {
+        type: 'bar',
+        data: [2, 4, 6, 8],
+        coordinateSystem: 'polar',
+        name: 'B',
+        stack: 'a'
+    }, {
+        type: 'bar',
+        data: [1, 2, 3, 4],
+        coordinateSystem: 'polar',
+        name: 'C',
+        stack: 'a'
+    }],
+    legend: {
+        show: true,
+        data: ['A', 'B', 'C']
+    }
+}
+                    }
+                },
+                props:{
+                  
+                },
+                mixins:[root],
+                
+                methods:{},
+                watch:{},
+                computed:{},
+                beforeCreate(){
+                    let ctx = this;
+                    
+                },
+                created(){
+                    let ctx = this;
+                    
+                    
+                    /**bind**/ctx.$store.commit("registerBind",{ vueObj:this, data:this.echartConfig, dataStr:"echartConfig", wid:1565088728187, modelKey:"configs" });/**bind over**/
+                },
+                beforeMount(){
+                    let ctx = this;
+                    
+                },
+                mounted(){
+                    let ctx = this;
+                    
+                    
+                },
+                beforeUpdate(){
+                    let ctx = this;
+                    
+                },
+                updated(){
+                    let ctx = this;
+                    
+                },
+                beforeDestroy(){ 
+                    let ctx = this;
+                        
+                },
+                destroyed(){
+                    let ctx = this;
+                        
+                },
+                deactivated(){
+                  /**页面切出**/
+                  let ctx = this;
+                  
+                },
+                activated(){
+                  /**页面切入**/
+                  let ctx = this;
+                  
+                },
+
+            };
+        </script>
+<style lang="scss">
+
+</style>
