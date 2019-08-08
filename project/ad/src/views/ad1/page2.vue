395578a59efe73ccfa4965c6458f1397266244a0
ZhangErin <31231134+ZhangErin@users.noreply.github.com>
2019-08-08 19:34:33 +0800

    添加ad页面和修改ad项目vue文件的编译

---
+++ project/ad/src/views/ad1/page2.vue
@@ -0,0 +1,70 @@
+<template><v2-ctn-xy :wid="`root`"><v2-component-text class="V2Widget" id="1565262964718" :wid="`1565262964718`" :index="0" :pid="`root`"></v2-component-text><v2-component-text class="V2Widget" id="1565262934532" :wid="`1565262934532`" :index="1" :pid="`root`"></v2-component-text></v2-ctn-xy></template>
+<script>
+            import {root} from '@/ideLib/mixin/v2-view';
+            
+            export default{
+                data(){
+                    return{
+                        "CONTENT":{structure:{"id":"root","component":"v2EditorXYCtn","style":{"width":"100%","height":"100%"},"children":[{"style":{"textStyle":{"text-align":"","margin":"","padding":"","height":"","width":"","line-height":"","font-family":"","font-size":"","font-weight":"","font-style":"","color":"","text-decoration":""}},"text":"你的赞同   我的赞同","editable":false,"ctnClass":"","customClass":"","__type":"","__loopTarget":"","__capacity":"1","component":"v2-component-text","href":"v2-component-text","children":[],"events":{},"commonStyle":{"position":"absolute","top":"130px","left":"436px","color":"#04bebd","font-size":"80px"},"id":1565262964718,"pid":"root","animate":{"name":"","duration":0,"delay":0,"classCode":""}},{"style":{"textStyle":{"text-align":"","margin":"","padding":"","height":"","width":"","line-height":"","font-family":"","font-size":"","font-weight":"","font-style":"","color":"","text-decoration":""}},"text":"专业的金融解决方案提供商","editable":false,"ctnClass":"","customClass":"","__type":"","__loopTarget":"","__capacity":"1","component":"v2-component-text","href":"v2-component-text","children":[],"events":{},"commonStyle":{"position":"absolute","top":"275px","left":"600px","font-size":"30px","color":"#607D8B"},"id":1565262934532,"pid":"root","animate":{"name":"","duration":0,"delay":0,"classCode":""}}],"ctnStyle":[{"position":"","top":"","right":"","left":"","bottom":"","width":"","height":"","text-align":"","background-color":"","padding-top":"","padding-bottom":"","padding-left":"","padding-right":"","margin-top":"","margin-bottom":"","margin-left":"","margin-right":"","border-color":"","border-width":"","border-style":""},{"position":"","top":"","right":"","left":"","bottom":"","width":"","height":"","text-align":"","background-color":"","padding-top":"","padding-bottom":"","padding-left":"","padding-right":"","margin-top":"","margin-bottom":"","margin-left":"","margin-right":"","border-color":"","border-width":"","border-style":""}]}},
+                        
+                    }
+                },
+                props:{
+                  
+                },
+                mixins:[root],
+                
+                methods:{},
+                watch:{},
+                computed:{},
+                beforeCreate(){
+                    let ctx = this;
+                    
+                },
+                created(){
+                    let ctx = this;
+                    
+                    
+                    /**bind**//**bind over**/
+                },
+                beforeMount(){
+                    let ctx = this;
+                    
+                },
+                mounted(){
+                    let ctx = this;
+                    
+                    
+                },
+                beforeUpdate(){
+                    let ctx = this;
+                    
+                },
+                updated(){
+                    let ctx = this;
+                    
+                },
+                beforeDestroy(){ 
+                    let ctx = this;
+                        
+                },
+                destroyed(){
+                    let ctx = this;
+                        
+                },
+                deactivated(){
+                  /**页面切出**/
+                  let ctx = this;
+                  
+                },
+                activated(){
+                  /**页面切入**/
+                  let ctx = this;
+                  
+                },
+
+            };
+        </script>
+<style lang="scss">
+
+</style>
