704448fa5c8aaf7817bb170b4469157c0368a17b
zhucheng <zhucheng@agree.com.cn>
2019-08-08 18:17:23 +0800

    A test editor

---
+++ project/@v2-components/v2-XXXXX/index.vue
@@ -0,0 +1,17 @@
+<template>
+        <div
+        class="V2XXXXX V2ContainerBlock"
+        :class="`${model.thisClasss}`"
+        ref="wrap"
+        :style="`width:${model.width};height:${model.height};background:${model.background};`"
+    >
+        <slot>
+
+        </slot>
+    </div>
+</template>
+<script>
+    export default{
+        
+    }
+</script>
