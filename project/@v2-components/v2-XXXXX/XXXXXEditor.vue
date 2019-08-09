1b253938b79992513cecd44c84853ae71b6fe071
zhucheng <zhucheng@agree.com.cn>
2019-08-09 16:06:49 +0800

    首页布局 && 一个可控块容器

--- project/@v2-components/v2-XXXXX/XXXXXEditor.vue
+++ project/@v2-components/v2-XXXXX/XXXXXEditor.vue
@@ -1,20 +1,15 @@
-<!--针对切换组件设计的编辑器
-    其特性是：
-    1、有多个编辑区域
-    2、同一时间只有一个编辑区域显示
--->
 <template>
   <div>
-    <el-tabs v-model="activeTab" type="card" editable @edit="handleTabsEdit">
-      <el-tab-pane
-        :key="`${index}`"
-        v-for="(item, index) in model.children"
-        :label="`页面${index}`"
-        :name="`${index}`"
-      >
-        <component :is="component(index)" :wid="widgetId(index)" :index="index" :pid="model.id"></component>
-      </el-tab-pane>
-    </el-tabs>
+    <!-- <template v-for="(lyt,index) of childComps">
+      <component
+        :is="component(index)"
+        :key="index"
+        :wid="widgetId(index)"
+        :index="index"
+        :pid="model.id"
+      ></component>
+    </template> -->
+    <component :is="component(0)" :wid="widgetId(0)" :index="0" :pid="model.id"></component>
   </div>
 </template>
 <script>
@@ -58,32 +53,17 @@ export default {
         if (item) return item.component;
         return "v2Empty";
       };
+    },
+    childComps() {
+      !this.model.children && (this.model.children = []);
+
+      return this.model.children;
     }
   },
-  methods: {
-    handleTabsEdit(targetName, action) {
-      if (action === "add") {
-        this.model.children = this.model.children || [];
-        this.activeTab = this.model.children.length - 1 + "";
-        this.$store.commit("$add.child", {
-          parent: this.model,
-          index: targetName,
-          model: {
-            id: new Date().valueOf() + ((Math.random() * 100000) | 0),
-            pid: this.model.id,
-            component: "v2Container",
-            layout: [100],
-            position: "row",
-            children: [null]
-          }
-        });
-      }
-      if (action === "remove") {
-        this.model.children.splice(targetName, 1);
-        this.activeTab = targetName - 1 + "";
-      }
-    }
-  }
+  methods: {}
 };
 </script>
+<style lang="scss">
+
+</style>
 
---
+++ project/@v2-components/v2-ctn-controllable/ControllableEditor.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="V2Controllable" ref="wrap"></div>
+</template>
+<script>
+export default {};
+</script>
+
+<style>
+</style>
