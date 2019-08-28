74fd4541d30c0294063ffb95589f94fb7c5bcc9c
KinSueng <594610825@qq.com>
2019-08-28 09:46:12 +0800

    修改找不到路由的问题
    修改找不到路由的问题

--- server/util/static-router.js
+++ server/util/static-router.js
@@ -27,24 +27,31 @@ async function get_Route_FilePath(ctx, data) {
 }
 
 async function toSend(ctx, dir, router) {
-    const index = (router && ctx.request.url.lastIndexOf(router) +( router.length))||0
-    const fileUrl = ctx.request.url.substring(index)
-    let filePath = Path.join(dir, fileUrl)
-    if (filePath.indexOf('?') !== -1) {
-        filePath = filePath.split('?')[0];
-    }
-    const isFile = await new Promise((resolve) => {
-        fs.stat(filePath, (err, stats) => {
-            if (err) {
-                resolve(null)
-            }
-            else {
-                resolve(stats.isFile());
-            }
+    try{
+        const index = (router && ctx.request.url.lastIndexOf(router) +( router.length))||0
+        const fileUrl = ctx.request.url.substring(index)
+        let filePath = Path.join(dir, fileUrl)
+        if (filePath.indexOf('?') !== -1) {
+            filePath = filePath.split('?')[0];
+        }
+        const isFile = await new Promise((resolve) => {
+            fs.stat(filePath, (err, stats) => {
+                if (err) {
+                    resolve(null)
+                }
+                else {
+                    resolve(stats.isFile());
+                }
+            })
         })
-    })
-    if (isFile) {
-        await send(ctx, filePath, { root: '/' });
+        if (isFile) {
+            await send(ctx, filePath, { root: '/' });
+        }
+    }catch(e){
+        return new Promise(r=>{
+            ctx.body = 'Not Found';
+            r();
+        })
     }
 }
 
