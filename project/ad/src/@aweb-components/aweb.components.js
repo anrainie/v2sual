756965d9d469e96f7e014aa5dbfcd61a3b6a854c
xianhongquan <525238521@qq.com>
2019-09-04 14:22:39 +0800

    上海VDA基础页面

--- project/ad/src/@aweb-components/aweb.components.js
+++ project/ad/src/@aweb-components/aweb.components.js
@@ -1,5 +1,5 @@
         
-        import v2ComponentBtn  from '@v2-components/v2-component-btn';
+import v2ComponentBtn  from '@v2-components/v2-component-btn';
 import v2ComponentCalendar  from '@v2-components/v2-component-calendar';
 import v2ComponentCard  from '@v2-components/v2-component-card';
 import v2ComponentCarousel  from '@v2-components/v2-component-carousel';
@@ -10,17 +10,11 @@ import v2ComponentSteps  from '@v2-components/v2-component-steps';
 import v2ComponentImage  from '@v2-components/v2-component-image';
 import v2ComponentTable  from '@v2-components/v2-component-table';
 import v2ComponentTabs  from '@v2-components/v2-component-tabs';
-import v2ComponentTable  from '@v2-components/v2-component-table';
-import v2ComponentSteps  from '@v2-components/v2-component-steps';
 import v2ComponentTextValue  from '@v2-components/v2-component-text-value';
 import v2CtnOrder  from '@v2-components/v2-ctn-order';
 import v2EditorOrderCtn  from '@v2-components/v2-ctn-order/orderCtnEditor.vue';
 import v2ComponentTree  from '@v2-components/v2-component-tree';
 import v2ComponentText  from '@v2-components/v2-component-text';
-import v2CtnXy  from '@v2-components/v2-ctn-xy';
-import v2EditorXYCtn  from '@v2-components/v2-ctn-xy/xyCtnEditor.vue';
-import v2CtnOrder  from '@v2-components/v2-ctn-order';
-import v2EditorOrderCtn  from '@v2-components/v2-ctn-order/orderCtnEditor.vue';
 import v2CtnXy  from '@v2-components/v2-ctn-xy';
 import v2EditorXYCtn  from '@v2-components/v2-ctn-xy/xyCtnEditor.vue';
 import v2EchartBar  from '@v2-components/v2-echart-bar';
@@ -31,7 +25,6 @@ import v2FormDatepicker  from '@v2-components/v2-form-datepicker';
 import v2FormEditor  from '@v2-components/v2-form-editor';
 import v2FormInput  from '@v2-components/v2-form-input';
 import v2FormInputnumber  from '@v2-components/v2-form-inputnumber';
-import v2FormInput  from '@v2-components/v2-form-input';
 import v2FormRadio  from '@v2-components/v2-form-radio';
 import v2FormSelect  from '@v2-components/v2-form-select';
 import v2FormSlider  from '@v2-components/v2-form-slider';
@@ -50,101 +43,96 @@ import v2EditorSwitch  from '@v2-components/v2-switchable/switchEditor.vue';
 import v2ComponentWeather  from '../v2Components/v2-component-weather';
 import v2Vda3dpie  from '../v2Components/v2-vda-3dpie';
 import v2VdaChart  from '../v2Components/v2-vda-chart';
-import v2Vda3dpie  from '../v2Components/v2-vda-3dpie';
-import v2ComponentWeather  from '../v2Components/v2-component-weather';
 import v2VdaPrettyGlobal  from '../v2Components/v2-vda-pretty-global';
 import v2ComponentDigitroll  from '../v2Components/v2-component-digitroll'
-  
-        import getValue  from '../../pipe/getValue';
+
+	import getValue  from '../../pipe/getValue';
 import mapping  from '../../pipe/mapping';
 import vda  from '../../pipe/vda';
 import filter  from '../../pipe/filter';
 import animate  from '@v2-pipe/animate';
 import close  from '@v2-pipe/close';
 import animateKill  from '@v2-pipe/animateKill';
-import animate  from '@v2-pipe/animate';
 import getAfaData  from '@v2-pipe/getAfaData';
 import getExcelData  from '@v2-pipe/getExcelData';
 import map  from '@v2-pipe/map';
 import gt  from '@v2-pipe/gt';
-import getExcelData  from '@v2-pipe/getExcelData';
 import open  from '@v2-pipe/open';
-import map  from '@v2-pipe/map'
-  
-        let app = {
-          getValue,
-	mapping,
-	vda,
-	filter,
-	animate,
-	close,
-	animateKill,
-	animate,
-	getAfaData,
-	getExcelData,
-	map,
-	gt,
-	getExcelData,
-	open,
-	map
-        };
-  
-        window.pipe = app;
-  
-        export default {
-          v2ComponentBtn,
-	v2ComponentCalendar,
-	v2ComponentCard,
-	v2ComponentCarousel,
-	v2ComponentEtable,
-	v2ComponentIcon,
-	v2ComponentEchart,
-	v2ComponentSteps,
-	v2ComponentImage,
-	v2ComponentTable,
-	v2ComponentTabs,
-	v2ComponentTable,
-	v2ComponentSteps,
-	v2ComponentTextValue,
-	v2CtnOrder,
-	v2EditorOrderCtn,
-	v2ComponentTree,
-	v2ComponentText,
-	v2CtnXy,
-	v2EditorXYCtn,
-	v2CtnOrder,
-	v2EditorOrderCtn,
-	v2CtnXy,
-	v2EditorXYCtn,
-	v2EchartBar,
-	v2EchartLine,
-	v2FormCascader,
-	v2FormCheckbox,
-	v2FormDatepicker,
-	v2FormEditor,
-	v2FormInput,
-	v2FormInputnumber,
-	v2FormInput,
-	v2FormRadio,
-	v2FormSelect,
-	v2FormSlider,
-	v2FormSwitch,
-	v2FormTimepicker,
-	v2FormTransfer,
-	v2FormUpload,
-	v2Layout,
-	v2LayoutEditor,
-	v2MobileButton,
-	v2MobileImage,
-	v2MobileInput,
-	v2MobileSteps,
-	v2Switchable,
-	v2EditorSwitch,
-	v2ComponentWeather,
-	v2Vda3dpie,
-	v2VdaChart,
-	v2Vda3dpie,
-	v2ComponentWeather,
-	v2VdaPrettyGlobal,
-	v2ComponentDigitroll
-        }
+
+	let app = {
+		getValue,
+mapping,
+vda,
+filter,
+animate,
+close,
+animateKill,
+animate,
+getAfaData,
+getExcelData,
+map,
+gt,
+getExcelData,
+open,
+map
+	};
+
+	window.pipe = app;
+
+	export default {
+		v2ComponentBtn,
+		v2ComponentCalendar,
+		v2ComponentCard,
+		v2ComponentCarousel,
+		v2ComponentEtable,
+		v2ComponentIcon,
+		v2ComponentEchart,
+		v2ComponentSteps,
+		v2ComponentImage,
+		v2ComponentTable,
+		v2ComponentTabs,
+		v2ComponentTable,
+		v2ComponentSteps,
+		v2ComponentTextValue,
+		v2CtnOrder,
+		v2EditorOrderCtn,
+		v2ComponentTree,
+		v2ComponentText,
+		v2CtnXy,
+		v2EditorXYCtn,
+		v2CtnOrder,
+		v2EditorOrderCtn,
+		v2CtnXy,
+		v2EditorXYCtn,
+		v2EchartBar,
+		v2EchartLine,
+		v2FormCascader,
+		v2FormCheckbox,
+		v2FormDatepicker,
+		v2FormEditor,
+		v2FormInput,
+		v2FormInputnumber,
+		v2FormInput,
+		v2FormRadio,
+		v2FormSelect,
+		v2FormSlider,
+		v2FormSwitch,
+		v2FormTimepicker,
+		v2FormTransfer,
+		v2FormUpload,
+		v2Layout,
+		v2LayoutEditor,
+		v2MobileButton,
+		v2MobileImage,
+		v2MobileInput,
+		v2MobileSteps,
+		v2Switchable,
+		v2EditorSwitch,
+		v2ComponentWeather,
+		v2Vda3dpie,
+		v2VdaChart,
+		v2Vda3dpie,
+		v2ComponentWeather,
+		v2VdaPrettyGlobal,
+		v2ComponentDigitroll
+	}
