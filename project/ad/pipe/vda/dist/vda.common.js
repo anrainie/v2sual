51d7dd6ef515bdf2eade968296be78c72b9e0759
gavin <gavin@git.com>
2019-09-07 15:45:46 +0800

    修复
    重新打包修复bug

--- project/ad/pipe/vda/dist/vda.common.js
+++ project/ad/pipe/vda/dist/vda.common.js
@@ -805,7 +805,8 @@ var queryDmData = 'visualThemeController/queryDmData';
 var queryNewDmData = 'visualThemeController/queryNewDmData';
 var createPanelUrl = 'visualConfPanelController/save/panel';
 var username = 'admin';
-var vda_password = '888888';
+var vda_password = '888888'; //'agreexian!';
+
 var token = '';
 var fakeData = {
   '网点树': {
@@ -1806,165 +1807,32 @@ module.exports = __webpack_require__("a125") ? function (object, key, value) {
 /***/ "639b":
 /***/ (function(module, exports) {
 
-var host = 'v1/ds/dvms/vda';
-var signIn = 'loginController/signIn';
-var apiList = 'visualDataModelController/queryTreeDataModels';
-var dataPreview = 'visualDataModelController/dataPreview';
-var panelSource = 'visualConfPanelController/info/panel';
-var queryDmData = 'visualThemeController/queryDmData';
-var queryNewDmData = 'visualThemeController/queryNewDmData';
-var createPanelUrl = 'visualConfPanelController/save/panel';
-var username = 'admin';
-var vda_password = '888888'; //'agreexian!';
+// shim for using process in browser
+var process = module.exports = {};
 
-var token = '';
-var fakeData = {
-  '网点树': {
-    "params": [{
-      "requestid": "27CF0EDB-981B-4580-9A19-A23AADE92B64",
-      "cols": ["_orgname"]
-    }],
-    "content": {
-      '27CF0EDB-981B-4580-9A19-A23AADE92B64': [{
-        "_orgname": "渠道管理部"
-      }, {
-        "_orgname": "平台管理部"
-      }, {
-        "_orgname": "运行管理部"
-      }, {
-        "_orgname": "系统应用管理部"
-      }, {
-        "_orgname": "杭州分行"
-      }, {
-        "_orgname": "杭州分行营业部"
-      }, {
-        "_orgname": "市北分行"
-      }, {
-        "_orgname": "杨浦支行"
-      }, {
-        "_orgname": "杨浦支行营业部"
-      }, {
-        "_orgname": "闸北支行"
-      }, {
-        "_orgname": "零售业务部"
-      }]
-    }
-  },
-  "开户趋势": {
-    "params": [{
-      "requestid": "518B3F39-B17B-486D-B799-779F0CD98FC5",
-      "datatype": "1",
-      "moduleid": "0f25a46ec83a38088909c16269edb30e",
-      "statcontent": {},
-      "filter": "",
-      "modelfilter": "",
-      "order": "",
-      "cols": ["开会数昨日同比"],
-      "limit": ""
-    }],
-    "content": {
-      "518B3F39-B17B-486D-B799-779F0CD98FC5": [{
-        "_openAccountCount": 351
-      }, {
-        "_openAccountCount": 2
-      }, {
-        "_openAccountCount": 3666
-      }, {
-        "_openAccountCount": 1679
-      }, {
-        "_openAccountCount": 2244
-      }, {
-        "_openAccountCount": 180
-      }, {
-        "_openAccountCount": 682
-      }, {
-        "_openAccountCount": 5248
-      }, {
-        "_openAccountCount": 49
-      }]
-    }
-  },
-  "自助设备异常信息": {
-    "params": [{
-      "requestid": "323A3D01-6CA5-4C2C-9981-2B065AC9020B",
-      "cols": ["_DID", "_DT", "_TBID", "_DSN", "TBN"]
-    }],
-    "content": {
-      "323A3D01-6CA5-4C2C-9981-2B065AC9020B": [{
-        "_TBID": "CN0010121",
-        "_DID": "18003055",
-        "_DSN": "正常",
-        "_TBN": "杨浦支行营业部",
-        "_DT": "CDS"
-      }, {
-        "_TBID": "CN0010413",
-        "_DID": "18003054",
-        "_DSN": "正常",
-        "_TBN": "北站支行",
-        "_DT": "CDS"
-      }, {
-        "_TBID": "CN0010121",
-        "_DID": "18003053",
-        "_DSN": "正常",
-        "_TBN": "杨浦支行营业部",
-        "_DT": "CDS"
-      }, {
-        "_TBID": "CN0010413",
-        "_DID": "18003058",
-        "_DSN": "离线",
-        "_TBN": "北站支行",
-        "_DT": "ATM"
-      }, {
-        "_TBID": "CN0010413",
-        "_DID": "18003057",
-        "_DSN": "卡纸",
-        "_TBN": "杨浦支行营业部",
-        "_DT": "CDS"
-      }]
-    }
-  },
-  '当前排队': {
-    "params": [{
-      "requestid": "0A7C1993-E24C-405F-AD67-24D860F5CBAD",
-      "cols": ["_queuedCount"]
-    }],
-    "content": {
-      '0A7C1993-E24C-405F-AD67-24D860F5CBAD': [{
-        "_queuedCount": 11
-      }, {
-        "_queuedCount": 11
-      }, {
-        "_queuedCount": 11
-      }, {
-        "_queuedCount": 11
-      }, {
-        "_queuedCount": 11
-      }, {
-        "_queuedCount": 11
-      }]
-    }
-  },
-  "已处理": {
-    "params": [{
-      "requestid": "F6B35B6C-3269-4ACB-941A-2C5E67B12D78",
-      "cols": ["_calledCount"]
-    }],
-    "content": {
-      'F6B35B6C-3269-4ACB-941A-2C5E67B12D78': [{
-        "_calledCount": 11
-      }, {
-        "_calledCount": 11
-      }, {
-        "_calledCount": 11
-      }, {
-        "_calledCount": 11
-      }, {
-        "_calledCount": 11
-      }, {
-        "_calledCount": 11
-      }, {
-        "_calledCount": 11
-      }]
+// cached from whatever global is present so that test runners that stub it
+// don't break things.  But we need to wrap it in a try catch in case it is
+// wrapped in strict mode code which doesn't define any globals.  It's inside a
+// function because try/catches deoptimize in certain engines.
+
+var cachedSetTimeout;
+var cachedClearTimeout;
+
+function defaultSetTimout() {
+    throw new Error('setTimeout has not been defined');
+}
+function defaultClearTimeout () {
+    throw new Error('clearTimeout has not been defined');
+}
+(function () {
+    try {
+        if (typeof setTimeout === 'function') {
+            cachedSetTimeout = setTimeout;
+        } else {
+            cachedSetTimeout = defaultSetTimout;
+        }
+    } catch (e) {
+        cachedSetTimeout = defaultSetTimout;
     }
     try {
         if (typeof clearTimeout === 'function') {
