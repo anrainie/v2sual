95df9a195467df0820ee1f89ad8175732237d0f5
anrainie <anrainie@163.com>
2019-09-17 14:47:10 +0800

    fixed delete bugs

--- project/runtime/src/views/test/empty.vue
+++ project/runtime/src/views/test/empty.vue
@@ -1,20 +1,29 @@
-<template><v2container :wid="`root`"><el-row class="V2ContainerBlock borderBox dashBorder" style="height:100%;width:100%;" key="0"><v2-component-image class="V2Widget" id="1565256890715" :wid="`1565256890715`" :index="0" :pid="`root`"></v2-component-image></el-row></v2container></template>
+<template><v2container :wid="`root`"><el-row class="V2ContainerBlock" style="height:20%;" key="0"><v2empty class="V2Empty" id="root-0" :wid="`root-0`" :index="0" :pid="`root`"></v2empty></el-row><el-row class="V2ContainerBlock" style="height:80%;" key="1"><v2container id="1568628017920" :wid="`1568628017920`" :index="1" :pid="`root`"><el-col class="V2ContainerBlock" style="width:20%;" key="0"><v2empty class="V2Empty" id="1568628017920-0" :wid="`1568628017920-0`" :index="0" :pid="`1568628017920`"></v2empty></el-col><el-col class="V2ContainerBlock" style="width:80%;" key="1"><v2empty class="V2Empty" id="1568628017920-1" :wid="`1568628017920-1`" :index="1" :pid="`1568628017920`"></v2empty></el-col></v2container></el-row></v2container></template>
 <script>
+            import  {mixins} from '@v2-lib/vue.spa.plugin';
             import {root} from '@v2-lib/v2sual/mixin/v2-view';
             
             export default{
                 data(){
                     return{
-                        "CONTENT":{structure:{"id":"root","component":"v2Container","direction":"row","layout":[100],"style":{"width":"100%","height":"100%"},"data":{},"children":[{"src":"./img/071201.png","height":"100%","width":"100%","ctnClass":"","customClass":"","__type":"","__loopTarget":"","__capacity":"1","component":"v2-component-image","href":"v2-component-image","children":[],"events":{},"id":1565256890715,"pid":"root","commonStyle":{"position":"","top":"","right":"","left":"","bottom":"","width":"","height":"","padding-top":"","padding-bottom":"","padding-left":"","padding-right":"","margin-top":"","margin-bottom":"","margin-left":"","margin-right":"","font-size":"","color":""}}],"events":{},"ctnStyle":[{"position":"","top":"","right":"","left":"","bottom":"","width":"","height":"","text-align":"","background-color":"","padding-top":"","padding-bottom":"","padding-left":"","padding-right":"","margin-top":"","margin-bottom":"","margin-left":"","margin-right":"","border-color":"","border-width":"","border-style":""}]}},
+                        "CONTENT":{structure:{"id":"root","component":"v2Container","direction":"row","layout":[20,80],"style":{"width":"100%","height":"100%"},"data":{},"children":[null,{"layout":[20,80],"direction":"col","component":"v2-layout","children":[null,null],"id":1568628017920,"pid":1568627970158,"ctnStyle":[{},{}]}],"events":{},"ctnStyle":[{},{}],"realSize":["%","%"],"commonStyle":{"animation-duration":"1s"},"animate":["","animated"]}},
                         
                     }
                 },
                 props:{
                   
                 },
-                mixins:[root],
+                mixins:[root,mixins],
                 
-                methods:{},
+                methods:{
+                  _source_init(){
+                    let ctx = this;
+                    window.pipe.vda.ready().then(() =>{
+                      
+                    });
+                  },
+                  
+                },
                 watch:{},
                 computed:{},
                 beforeCreate(){
@@ -24,8 +33,9 @@
                 created(){
                     let ctx = this;
                     
-                    
+                    ctx._source_init();
                     /**bind**//**bind over**/
+
                 },
                 beforeMount(){
                     let ctx = this;
